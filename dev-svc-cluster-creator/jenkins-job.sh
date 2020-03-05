2bd296c6ee7b3097fe7b928aaac1d7751c4f90cb
Pavel Macík <pavel.macik@gmail.com>
2020-03-05 19:44:17 +0100

    [dev-svc-cluster-creator] Add functions for installing toolchain operator (incl. a regular user).

--- dev-svc-cluster-creator/jenkins-job.sh
+++ dev-svc-cluster-creator/jenkins-job.sh
@@ -15,6 +15,62 @@ set -eo pipefail
 # GIST_API_TOKEN="..."
 # SLACK_API_TOKEN="..."
 
+
+function print_operator_subscription {
+    PACKAGE_NAME=$1
+    OPSRC_NAME=$2
+    CHANNEL=$3
+
+    CSV_VERSION=$(oc get packagemanifest $PACKAGE_NAME -o jsonpath='{.status.channels[?(@.name == "'$CHANNEL'")].currentCSV}')
+    sed -e 's,REPLACE_CSV_VERSION,'$CSV_VERSION',g' ./subscription.template.yaml \
+    | sed -e 's,REPLACE_CHANNEL,'$CHANNEL',g' \
+    | sed -e 's,REPLACE_OPSRC_NAME,'$OPSRC_NAME',g' \
+    | sed -e 's,REPLACE_NAME,'$PACKAGE_NAME',g';
+}
+
+function install_operator_subscription {
+    if [[ ! -z $(oc get packagemanifest | grep $1) ]]; then
+        print_operator_subscription $1 $2 $3 | oc apply --wait -f -
+    else
+        echo "ERROR: packagemanifest $1 not found";
+        exit 1;
+    fi
+}
+
+function install_toolchain_operator {
+    NAME=codeready-toolchain-operator
+    OPSRC_NAME=codeready-toolchain-operator
+    CHANNEL=alpha
+
+    print_operator_subscription $NAME $OPSRC_NAME $CHANNEL | oc apply --wait  -f -
+}
+
+function add_user {
+    HTPASSWD_FILE="./htpass"
+    USERNAME="consoledeveloper"
+    USERPASS="developer"
+    HTPASSWD_SECRET="htpasswd-$USERNAME-secret"
+
+    OC_USERS_LIST="$(oc get users)"
+    if echo "${OC_USERS_LIST}" | grep -q "${USERNAME}"; then
+        echo -e "\n\033[0;32m \xE2\x9C\x94 User consoledeveloper already exists \033[0m\n"
+        exit;
+    fi
+    htpasswd -cb $HTPASSWD_FILE $USERNAME $USERPASS
+
+    oc get secret $HTPASSWD_SECRET -n openshift-config &> /dev/null \
+    || oc create secret generic ${HTPASSWD_SECRET} --from-file=htpasswd=${HTPASSWD_FILE} -n openshift-config
+
+    sed -e "s,HTPASSWD_SECRET,${HTPASSWD_SECRET},g" ./oauth.template.yaml | oc apply -f -
+
+    sleep 10s
+    oc create clusterrolebinding ${USERNAME}_role1 --clusterrole=self-provisioner --user=${USERNAME} || echo clusterrolebinding ${USERNAME}_role1 exists
+    oc create clusterrolebinding ${USERNAME}_role2 --clusterrole=view --user=${USERNAME} || echo clusterrolebinding ${USERNAME}_role2 exists
+    sleep 15s
+    echo -e "\n\e[1;35m User consoledeveloper created with the password developer. Type the below\e[0m \n"
+    echo -e "\n\e[1;32m oc login -u\e[3m \e[1;36mconsoledeveloper\e[0m \e[1;32m-p\e[3m \e[1;36mdeveloper\e[0m \n"
+}
+
 cd $WORKSPACE
 
 wget -O oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/${OCP_RELEASE_DIR}/${OI_VERSION}/openshift-client-linux-${OI_VERSION}.tar.gz
@@ -49,7 +105,13 @@ description=$(ocp4-aws -i dev-svc | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
 
 export GIST_ADD_API="https://api.github.com/gists"'?'"access_token=$GIST_API_TOKEN"
 
-GIST=$(curl -L -XPOST -d "{\"description\":\"$description\",\"public\":false,\"files\":{\"kubeconfig\":{\"content\":\"$(sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' $OCP4_AWS_WORKSPACE/current/auth/kubeconfig)\"}}}" $GIST_ADD_API)
+export KUBECONFIG="$OCP4_AWS_WORKSPACE/current/auth/kubeconfig"
+
+GIST=$(curl -L -XPOST -d "{\"description\":\"$description\",\"public\":false,\"files\":{\"kubeconfig\":{\"content\":\"$(sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' $KUBECONFIG)\"}}}" $GIST_ADD_API)
+
+#TODO: check operator_install logs for failures. If a failure occurs, do not print message to slack.
+# log file = $OCP4_AWS_WORKSPACE/current/.openshift_install.log
+# success message = 'time="2020-03-05T14:05:07Z" level=info msg="Install complete!"'
 
 #--output
 
@@ -66,6 +128,11 @@ SLACK_MESSAGE="$SLACK_TEAM, Today's dev cluster:\n\`\`\`\n$(sed -E ':a;N;$!ba;s/
 echo "------"
 echo $SLACK_MESSAGE
 echo "------"
+
+# install toolchain-operator
+
+add_user
+install_toolchain_operator
 
 if [ "$POST_CLUSTER_INFO_ON_SLACK" == "true" ]; then
     curl -XPOST -H "Content-Type: application/json" -H "Authorization: Bearer $SLACK_API_TOKEN" -d "{\"channel\":\"#forum-os-dev-services\",\"link_names\":\"true\",\"as_user\":\"true\",\"text\":\"$SLACK_MESSAGE\"}" 'https://coreos.slack.com/api/chat.postMessage'
